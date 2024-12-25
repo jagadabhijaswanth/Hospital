5c3e720b2f774fe701208ffb503838bdbe7e4421
jagadabhijaswanth <jagadabhijaswanth@gmail.com>
2024-12-25 20:56:44 +0000

    tables created

--- Hospital.sql
+++ Hospital.sql
@@ -1,20 +1,8 @@
 -- Active: 1735154969910@@127.0.0.1@5432@postgres
+'''Creating schema called 'Hospital' '''
 CREATE SCHEMA Hospital;
-CREATE TABLE Patient_table(
-    Patient_id INT PRIMARY KEY,
-    Patient_Name VARCHAR(100),
-    Age INT,
-    Gender VARCHAR(10),
-    Allergies VARCHAR(100),
-    Contact_info VARCHAR(20),
-    Medical_history VARCHAR(1000)
-);
 
-
-drop TABLE doctor;
-DROP Table patient_table;
-
-
+''' creating Patient_table name table in Hospital schema '''
 CREATE TABLE hospital.Patient_table(
     Patient_id INT PRIMARY KEY,
     Patient_Name VARCHAR(100),
@@ -25,4 +13,34 @@ CREATE TABLE hospital.Patient_table(
     Medical_history VARCHAR(1000)
 );
 
-select * from hospital.patient_table;
+
+''' Creating Doctor_table in Hospital schema '''
+CREATE TABLE Hospital.Doctor_table(
+    Doctor_id INT PRIMARY KEY,
+    Doctor_Name VARCHAR(50),
+    Specilization VARCHAR(50),
+    Patients_managed INT
+);
+
+
+
+''' Creating Appointments_Table table in Hospital schema '''
+CREATE TABLE hospital.Appointments_table(
+    Appointment_id INT PRIMARY KEY,
+    Patient_id INT NOT NULL,
+    Doctor_id INT NOT NULL,
+    Date_Time TIMESTAMP,
+    Notes VARCHAR(100), 
+	FOREIGN KEY (patient_id) REFERENCES patient_table(patient_id),
+    FOREIGN KEY (doctor_id) REFERENCES doctor_table(doctor_id)
+);
+
+
+''' Creating Medications_table table in Hospital schema '''
+CREATE TABLE hospital.medication_table(
+    patient_id INT,
+    doctor_id INT,
+    medicines VARCHAR(20),
+    FOREIGN KEY (patient_id) REFERENCES patient_table(patient_id),
+    FOREIGN KEY (doctor_id) REFERENCES doctor_table(doctor_id)
+);
